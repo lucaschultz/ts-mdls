173a5623ef03dfcb6b9fcde5edb7c0024641e0dd
Luca Schultz <45899273+lucaschultz@users.noreply.github.com>
2022-11-22 16:31:04 +0100

    Remove example

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-import { cwd } from "process";
 import mdls from "./mdls";
 
 export default mdls;
-
-(async () => {
-  try {
-    const data = await mdls("./src/index.ts", [
-      "kMDItemUserTags",
-      "kMDItemFSCreationDate",
-    ]);
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-})();
